0171db78eb417725d8973da30fb31a206097e3b6
lnx00 <linch255@gmail.com>
2023-02-27 11:57:45 +0100

    Projekt eingerichtet

---
+++ AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/AusgabenVerwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.ausgabenverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusgabenVerwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AusgabenVerwaltungApplication.class, args);
+    }
+
+}
---
+++ AusgabenVerwaltung/src/test/java/de/hhu/ausgabenverwaltung/AusgabenVerwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.ausgabenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusgabenVerwaltungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

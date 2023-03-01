8ca60953f3bcb18dadd6839c78533932a6b545e6
lnx00 <linch255@gmail.com>
2023-03-01 11:11:35 +0100

    Berechne alle Salden + Test

--- AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/service/GruppenService.java
+++ AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/service/GruppenService.java
@@ -1,14 +1,12 @@
 package de.hhu.ausgabenverwaltung.service;
 
 import de.hhu.ausgabenverwaltung.domain.Gruppe;
+import de.hhu.ausgabenverwaltung.domain.Transaktion;
 import de.hhu.ausgabenverwaltung.domain.User;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class GruppenService {
@@ -17,14 +15,48 @@ public class GruppenService {
 		return new Gruppe(name, new ArrayList<>(), new ArrayList<>(List.of(ersteller)), new HashSet<>(),true);
 	}
 
-	public List<HashMap<User, BigDecimal>> alleSchuldenBerechnen(Gruppe gruppe){
-		List<HashMap<User,BigDecimal>> schulden = new ArrayList<>();
+	public HashMap<User, HashMap<User, BigDecimal>> alleSchuldenBerechnen(Gruppe gruppe){
+		HashMap<User, HashMap<User,BigDecimal>> schulden = new HashMap<>();
 
 		for (User mitglied : gruppe.getMitglieder()) {
-			schulden.add(gruppe.mussBezahlenVonUser(mitglied));
+			schulden.put(mitglied, gruppe.mussBezahlenVonUser(mitglied));
 		}
 		return schulden;
 
+	}
+
+	public HashMap<User, BigDecimal> berechneSalden(Gruppe gruppe) {
+		var alleSchulden = alleSchuldenBerechnen(gruppe);
+		HashMap<User, BigDecimal> schuldenSumme = new HashMap<>();
+
+		// Iteriere durch alle Spalten der Tabelle
+		for (var entry : alleSchulden.entrySet()) {
+			User aktuellerUser = entry.getKey();
+			BigDecimal summe = schuldenSumme.getOrDefault(aktuellerUser, BigDecimal.ZERO);
+
+			// Iteriere durch alle Zeilen der Tabelle
+			for (var userSchulden : entry.getValue().entrySet()) {
+				// Subtrahiere Schulden des Ziel Users
+				User zielUser = userSchulden.getKey();
+				BigDecimal zielSumme = schuldenSumme.getOrDefault(zielUser, BigDecimal.ZERO);
+				zielSumme = zielSumme.subtract(userSchulden.getValue());
+				schuldenSumme.put(zielUser, zielSumme);
+
+				// Addiere Einnahmen des aktuellen User
+				summe = summe.add(userSchulden.getValue());
+			}
+
+			schuldenSumme.put(aktuellerUser, summe);
+		}
+
+		return schuldenSumme;
+	}
+
+	public Set<Transaktion> berechneTransaktionen(Gruppe gruppe) {
+		Set<Transaktion> transaktionen = new HashSet<>();
+
+
+		return transaktionen;
 	}
 
 }
--- AusgabenVerwaltung/src/test/java/de/hhu/ausgabenverwaltung/service/GruppenServiceTest.java
+++ AusgabenVerwaltung/src/test/java/de/hhu/ausgabenverwaltung/service/GruppenServiceTest.java
@@ -36,10 +36,49 @@ class GruppenServiceTest {
 		Ausgabe ausgabe1 = new Ausgabe("ausgabe1", "Essen", new BigDecimal(10),user1,new ArrayList<>(List.of(user1,user2)));
 		Ausgabe ausgabe2 = new Ausgabe("ausgabe1", "Kino", new BigDecimal(30),user2,new ArrayList<>(List.of(user1,user2)));
 		Gruppe gruppe = new Gruppe("gruppe", new ArrayList<>(List.of(ausgabe1,ausgabe2)), new ArrayList<>(List.of(user1, user2)), new HashSet<>(),true);
-		//Act
-		List<HashMap<User, BigDecimal>> listVonSchuldigen =  gruppenService.alleSchuldenBerechnen(gruppe);
-		//Assert
-		assertThat(listVonSchuldigen).contains(new HashMap<>(Map.of(user1, new BigDecimal(15))), new HashMap<>(Map.of(user2, new BigDecimal(5))));
 
+		//Act
+		var listVonSchuldigen =  gruppenService.alleSchuldenBerechnen(gruppe);
+
+		//Assert
+		assertThat(listVonSchuldigen).containsEntry(user1, new HashMap<>(Map.of(user2, new BigDecimal(5))));
+		assertThat(listVonSchuldigen).containsEntry(user2, new HashMap<>(Map.of(user1, new BigDecimal(15))));
 	}
+
+	@Test
+	@DisplayName("Salden werden korrekt berechnet")
+	void berechneSalden() {
+		//Arrange
+		GruppenService gruppenService = new GruppenService();
+		User userA = new User("githubname1", "Jens");
+		User userB = new User("githubname2", "Bob");
+		User userC = new User("githubname3", "Peter");
+		User userD = new User("githubname4", "Heinz");
+
+		Ausgabe ausgabe1 = new Ausgabe("ausgabe1", "Ausgabe1", new BigDecimal(8),userA,new ArrayList<>(List.of(userB)));
+		Ausgabe ausgabe2 = new Ausgabe("ausgabe2", "Ausgabe2", new BigDecimal(5),userA,new ArrayList<>(List.of(userC)));
+		Ausgabe ausgabe4 = new Ausgabe("ausgabe4", "Ausgabe2", new BigDecimal(3),userB,new ArrayList<>(List.of(userA)));
+		Ausgabe ausgabe5 = new Ausgabe("ausgabe5", "Ausgabe2", new BigDecimal(7),userB,new ArrayList<>(List.of(userC)));
+		Ausgabe ausgabe6 = new Ausgabe("ausgabe6", "Ausgabe2", new BigDecimal(11),userB,new ArrayList<>(List.of(userD)));
+
+		Ausgabe ausgabe7 = new Ausgabe("ausgabe7", "Ausgabe2", new BigDecimal(10),userC,new ArrayList<>(List.of(userA)));
+		Ausgabe ausgabe8 = new Ausgabe("ausgabe8", "Ausgabe2", new BigDecimal(1),userC,new ArrayList<>(List.of(userB)));
+		Ausgabe ausgabe9 = new Ausgabe("ausgabe9", "Ausgabe2", new BigDecimal(6),userC,new ArrayList<>(List.of(userD)));
+
+		Ausgabe ausgabe10 = new Ausgabe("ausgabe10", "Ausgabe2", new BigDecimal(2),userD,new ArrayList<>(List.of(userA)));
+		Ausgabe ausgabe11 = new Ausgabe("ausgabe11", "Ausgabe2", new BigDecimal(5),userD,new ArrayList<>(List.of(userB)));
+		Ausgabe ausgabe12 = new Ausgabe("ausgabe12", "Ausgabe2", new BigDecimal(4),userD,new ArrayList<>(List.of(userC)));
+
+		Gruppe gruppe = new Gruppe("gruppe", new ArrayList<>(List.of(ausgabe1,ausgabe2,ausgabe4,ausgabe5,ausgabe6,ausgabe7,ausgabe8,ausgabe9,ausgabe10,ausgabe11,ausgabe12)), new ArrayList<>(List.of(userA, userB, userC, userD)), new HashSet<>(),true);
+
+		//Act
+		var alleSalden =  gruppenService.berechneSalden(gruppe);
+
+		// Assert
+		assertThat(alleSalden).containsEntry(userA, new BigDecimal(-2));
+		assertThat(alleSalden).containsEntry(userB, new BigDecimal(7));
+		assertThat(alleSalden).containsEntry(userC, new BigDecimal(1));
+		assertThat(alleSalden).containsEntry(userD, new BigDecimal(-6));
+	}
+
 }

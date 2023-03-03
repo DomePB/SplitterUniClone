89702ae0ca2c533eff249245b32a10b3d04fa3af
lnx00 <linch255@gmail.com>
2023-03-03 09:33:43 +0100

    GitHub Authentifizierung hinzugefügt

--- AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/web/WebController.java
+++ AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/web/WebController.java
@@ -1,4 +1,23 @@
 package de.hhu.ausgabenverwaltung.web;
 
+import de.hhu.ausgabenverwaltung.service.GruppenService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class WebController {
+
+    private final GruppenService service;
+
+    public WebController(GruppenService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello, World!";
+    }
+
 }

e22dfdc8ab1a48800b56b000ddc3a94998dce17f
Amjad Alwais <Amjad.Alwais@hhu.de>
2023-03-07 09:48:17 +0100

    ausgabe hinzufügen

--- AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/web/WebController.java
+++ AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/web/WebController.java
@@ -4,21 +4,24 @@ import de.hhu.ausgabenverwaltung.domain.Ausgabe;
 import de.hhu.ausgabenverwaltung.domain.Gruppe;
 import de.hhu.ausgabenverwaltung.domain.User;
 import de.hhu.ausgabenverwaltung.service.GruppenService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
 public class WebController {
 
     private final GruppenService service;
+
     public WebController(GruppenService service) {
         this.service = service;
     }
@@ -44,7 +47,7 @@ public class WebController {
     @GetMapping("/gruppe")
     public String gruppenUebersicht(@RequestParam UUID id, Model model) {
         try {
-           Gruppe gruppe = service.findById(id);
+            Gruppe gruppe = service.findById(id);
             model.addAttribute("gruppe", gruppe);
             return "gruppen-uebersicht";
         } catch (Exception e) {
@@ -52,4 +55,18 @@ public class WebController {
         }
     }
 
+    @PostMapping("/gruppe/ausgaben")
+    public String ausgabeHinzufuegen(@RequestParam UUID id, @RequestParam(name = "ausgabeName") String name, RedirectAttributes attrs,
+    HttpServletRequest http){
+        try {
+            Gruppe gruppe = service.findById(id);
+            gruppe.ausgabeHinzufuegen(new Ausgabe(name, "", BigDecimal.ONE, new User("guthub"),
+                    List.of()));
+            attrs.addFlashAttribute("id", gruppe.getId());
+
+            return "redirect:/";
+        } catch (Exception e) {
+            return "redirect:/";
+        }
+    }
 }

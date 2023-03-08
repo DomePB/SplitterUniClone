d0547070cf4cb49a8056b265558e308233beef99
lnx00 <linch255@gmail.com>
2023-03-08 14:32:54 +0100

    WebController: Index-Seite Test auf grün

--- AusgabenVerwaltung/src/test/java/de/hhu/ausgabenverwaltung/web/WebControllerTest.java
+++ AusgabenVerwaltung/src/test/java/de/hhu/ausgabenverwaltung/web/WebControllerTest.java
@@ -1,31 +1,26 @@
 package de.hhu.ausgabenverwaltung.web;
 
-import static com.tngtech.archunit.ArchConfiguration.get;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.config.http.MatcherType.mvc;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import de.hhu.ausgabenverwaltung.domain.Gruppe;
-import de.hhu.ausgabenverwaltung.domain.Transaktion;
 import de.hhu.ausgabenverwaltung.domain.User;
 import de.hhu.ausgabenverwaltung.helper.WithMockOAuth2User;
 import de.hhu.ausgabenverwaltung.service.GruppenService;
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.math.BigDecimal;
-import java.util.*;
 
 @AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(controllers = WebController.class)
@@ -57,7 +52,7 @@ class WebControllerTest {
     void test_3() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk())
-                .andExpect(model().attribute("user", new User("JoeSchmoe")));
+                .andExpect(model().attribute("user", "JoeSchmoe"));
     }
 
     @Test

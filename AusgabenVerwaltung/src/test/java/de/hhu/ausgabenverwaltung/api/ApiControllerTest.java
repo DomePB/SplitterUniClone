d4a7835c3eae1febe5679c8f55d3128b68dbb4be
Amjad Alwais <Amjad.Alwais@hhu.de>
2023-03-10 12:08:16 +0100

    Tests für Auslage Eintragen in API

--- AusgabenVerwaltung/src/test/java/de/hhu/ausgabenverwaltung/api/ApiControllerTest.java
+++ AusgabenVerwaltung/src/test/java/de/hhu/ausgabenverwaltung/api/ApiControllerTest.java
@@ -1,18 +1,21 @@
 package de.hhu.ausgabenverwaltung.api;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.hhu.ausgabenverwaltung.api.models.AuslagenModel;
 import de.hhu.ausgabenverwaltung.api.models.GruppeModel;
+import de.hhu.ausgabenverwaltung.domain.Ausgabe;
 import de.hhu.ausgabenverwaltung.domain.Gruppe;
 import de.hhu.ausgabenverwaltung.domain.User;
 import de.hhu.ausgabenverwaltung.service.GruppenService;
+
 import java.util.List;
 import java.util.Set;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,74 +30,121 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @WebMvcTest(controllers = ApiController.class)
 public class ApiControllerTest {
 
-  @Autowired
-  MockMvc mockMvc;
+    @Autowired
+    MockMvc mockMvc;
 
-  @MockBean
-  GruppenService service;
+    @MockBean
+    GruppenService service;
 
-  private static <T> Set<T> anySet() {
-    return any();
-  }
+    private static <T> Set<T> anySet() {
+        return any();
+    }
+
+    @Test
+    @DisplayName("Gruppe wird erstellt")
+    void test_1() throws Exception {
+        GruppeModel gruppeModel = new GruppeModel(null, "gruppenName", Set.of("A"), false, null);
+        String json = new ObjectMapper().writeValueAsString(gruppeModel);
+
+        Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(new User("user1")));
+
+        when(service.gruppeErstellen(anySet(), any())).thenReturn(gruppe);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/gruppen")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("Gruppen von User werden ausgegeben")
+    void test_2() throws Exception {
+        User user1 = new User("user1");
+        Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1));
+
+        when(service.gruppenVonUser(user1.githubHandle())).thenReturn(List.of(gruppe));
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/user/{githubHandle}/gruppen", user1.githubHandle()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @DisplayName("Gruppen-Info wird ausgegeben")
+    void test_3() throws Exception {
+        User user1 = new User("user1");
+        Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1));
+
+        when(service.findById(gruppe.getId())).thenReturn(gruppe);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/gruppen/{gruppenId}", gruppe.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @DisplayName("Gruppe wird geschlossen")
+    void test_4() throws Exception {
+        User user1 = new User("user1");
+        Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1));
+
+        doAnswer(invocation -> {
+            gruppe.schliessen();
+            return null;
+        }).when(service).gruppeSchliessen(gruppe.getId());
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/gruppen/{gruppenId}/schliessen", gruppe.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Ausgabe in der Gruppe wird ausgelegt")
+    void test_5() throws Exception {
+        User user1 = new User("user1");
+        User user2 = new User("user2");
+        AuslagenModel auslagenModel = new AuslagenModel("grund", "user1", 100, Set.of("user2"));
+        String json = new ObjectMapper().writeValueAsString(auslagenModel);
+        Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1, user2));
+        when(service.istOffen(gruppe.getId())).thenReturn(true);
+        doNothing().when(service).addAusgabe(any(), any());
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/gruppen/{gruppenId}/auslagen", gruppe.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)).andExpect(status().isCreated());
+    }
 
   @Test
-  @DisplayName("Gruppe wird erstellt")
-  void test_1() throws Exception {
-    GruppeModel gruppeModel = new GruppeModel(null, "gruppenName", Set.of("A"), false, null);
-    String json = new ObjectMapper().writeValueAsString(gruppeModel);
-
-    Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(new User("user1")));
-
-    when(service.gruppeErstellen(anySet(), any())).thenReturn(gruppe);
-
-    mockMvc.perform(MockMvcRequestBuilders.post("/api/gruppen")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(json))
-        .andExpect(status().isCreated());
-  }
-
-  @Test
-  @DisplayName("Gruppen von User werden ausgegeben")
-  void test_2() throws Exception {
+  @DisplayName("Ausgleich Konflikt wenn eine Gruppen bereits geschlossen ist")
+  void test_6() throws Exception {
     User user1 = new User("user1");
-    Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1));
-
-    when(service.gruppenVonUser(user1.githubHandle())).thenReturn(List.of(gruppe));
-
+    User user2 = new User("user2");
+    AuslagenModel auslagenModel = new AuslagenModel("grund", "user1", 100, Set.of("user2"));
+    String json = new ObjectMapper().writeValueAsString(auslagenModel);
+    Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1, user2));
+    when(service.istOffen(gruppe.getId())).thenThrow(new Exception(""));
+    doNothing().when(service).addAusgabe(any(), any());
     mockMvc.perform(
-            MockMvcRequestBuilders.get("/api/user/{githubHandle}/gruppen", user1.githubHandle()))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+            MockMvcRequestBuilders.post("/api/gruppen/{gruppenId}/auslagen", gruppe.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)).andExpect(status().isNotFound());
   }
 
   @Test
-  @DisplayName("Gruppen-Info wird ausgegeben")
-  void test_3() throws Exception {
+  @DisplayName("Json-Auslagen ist fehlerhaft")
+  void test_7() throws Exception {
     User user1 = new User("user1");
-    Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1));
-
-    when(service.findById(gruppe.getId())).thenReturn(gruppe);
-
+    User user2 = new User("user2");
+    Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1, user2));
+    when(service.istOffen(gruppe.getId())).thenReturn(true);
+    doNothing().when(service).addAusgabe(any(), any());
     mockMvc.perform(
-            MockMvcRequestBuilders.get("/api/gruppen/{gruppenId}", gruppe.getId()))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+            MockMvcRequestBuilders.post("/api/gruppen/{gruppenId}/auslagen", gruppe.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("")).andExpect(status().isBadRequest());
   }
 
-  @Test
-  @DisplayName("Gruppe wird geschlossen")
-  void test_4() throws Exception {
-    User user1 = new User("user1");
-    Gruppe gruppe = Gruppe.gruppeErstellen("gruppenName", Set.of(user1));
-
-    doAnswer(invocation -> {
-      gruppe.schliessen();
-      return null;
-    }).when(service).gruppeSchliessen(gruppe.getId());
-
-    mockMvc.perform(
-            MockMvcRequestBuilders.post("/api/gruppen/{gruppenId}/schliessen", gruppe.getId()))
-        .andExpect(status().isOk());
-  }
 
 }

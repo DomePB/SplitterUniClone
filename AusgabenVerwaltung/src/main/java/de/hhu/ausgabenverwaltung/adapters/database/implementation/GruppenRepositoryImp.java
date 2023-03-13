22de6414a49a477e1293348ecd2121e797c2343f
falih <mohssine.falih.adesso.de>
2023-03-13 13:11:53 +0100

    Schleifen in idiomatische Schleifen umgewandelt

--- AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/adapters/database/implementation/GruppenRepositoryImp.java
+++ AusgabenVerwaltung/src/main/java/de/hhu/ausgabenverwaltung/adapters/database/implementation/GruppenRepositoryImp.java
@@ -2,16 +2,14 @@ package de.hhu.ausgabenverwaltung.adapters.database.implementation;
 
 import de.hhu.ausgabenverwaltung.application.repo.GruppenRepository;
 import de.hhu.ausgabenverwaltung.domain.Gruppe;
-import de.hhu.ausgabenverwaltung.domain.Transaktion;
 import de.hhu.ausgabenverwaltung.domain.User;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -32,36 +30,27 @@ public class GruppenRepositoryImp implements GruppenRepository {
   public List<Gruppe> getGruppenvonUser(User user) {
     List<Gruppe> gruppenVonUser = new ArrayList<>();
 
-    for (Gruppe gruppe : gruppen.values()) {
-      if (gruppe.getMitglieder().contains(user)) {
-        gruppenVonUser.add(gruppe);
-      }
+    return gruppen.values().stream()
+            .filter(gruppe -> gruppe.getMitglieder().contains(user))
+            .collect(Collectors.toList());
     }
-    return gruppenVonUser;
-  }
+
 
   @Override
   public List<Gruppe> getOffeneGruppenVonUser(User user) {
     List<Gruppe> offeneGruppen = new ArrayList<>();
 
-    for (Gruppe gruppe : getGruppenvonUser(user)) {
-      if (gruppe.istOffen()) {
-        offeneGruppen.add(gruppe);
-      }
-    }
-    return offeneGruppen;
+    return getGruppenvonUser(user).stream()
+            .filter(Gruppe::istOffen)
+            .collect(Collectors.toList());
   }
 
   @Override
   public List<Gruppe> getGeschlosseneGruppenVonUser(User user) {
     List<Gruppe> offeneGruppen = new ArrayList<>();
-
-    for (Gruppe gruppe : getGruppenvonUser(user)) {
-      if (!gruppe.istOffen()) {
-        offeneGruppen.add(gruppe);
-      }
-    }
-    return offeneGruppen;
+    return getGruppenvonUser(user).stream()
+            .filter(gruppe -> !gruppe.istOffen())
+            .collect(Collectors.toList());
   }
 
   @Override
@@ -71,28 +60,10 @@ public class GruppenRepositoryImp implements GruppenRepository {
 
   @Override
   public Gruppe findById(UUID id) throws NoSuchElementException {
-      if (gruppen.containsKey(id)){
-        return gruppen.get(id);
-      }
-    throw new NoSuchElementException("Gruppe existiert nicht");
+    return gruppen.values().stream()
+            .filter(gruppe -> gruppe.getId().equals(id))
+            .findFirst()
+            .orElseThrow(() -> new NoSuchElementException("Gruppe existiert nicht"));
   }
 
-  public Map<Gruppe, Set<Transaktion>> getBeteiligteTransaktionen(User user) {
-    Map<Gruppe, Set<Transaktion>> userTransaktinen = new HashMap<>();
-    for (Gruppe gruppe : gruppen.values()) {
-      Set<Transaktion> temp = new HashSet<>();
-      if (gruppe.getMitglieder().contains(user)) {
-        Set<Transaktion> groupeTransaktionen =
-            gruppe.berechneTransaktionen(
-                gruppe.berechneSalden(gruppe.alleSchuldenBerechnen()));
-        for (Transaktion t : groupeTransaktionen) {
-          if (t.sender().equals(user) || t.empfaenger().equals(user)) {
-            temp.add(t);
-          }
-        }
-        userTransaktinen.put(gruppe, temp);
-      }
-    }
-    return userTransaktinen;
-  }
 }
